fa06177ac77964bb452bc6ca204ab9b1a754e4d9
Matthias Colin <matthias.colin@gmail.com>
2020-10-12 16:34:38 +0200

    config mysql

--- magasin_rest_orm/database.py
+++ magasin_rest_orm/database.py
@@ -1,4 +1,19 @@
 """
 database.py : config orm
 """
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "mysql://movie:password@localhost/magasin"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
   
