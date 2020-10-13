f5db8245784381da9063a6865176fac919e8cf15
Matthias Colin <matthias.colin@gmail.com>
2020-10-13 17:45:15 +0200

    end of tuesday

--- movie_rest_orm_api/crud.py
+++ movie_rest_orm_api/crud.py
@@ -5,11 +5,11 @@ manage CRUD and adapt model data from db to schema data to api rest
 
 from typing import Optional
 from sqlalchemy.orm import Session
-import sqlalchemy as alch
+from sqlalchemy import desc, extract, between
 
 import models, schemas
 
-
+# CRUD for Movie objects
 def get_movie(db: Session, movie_id: int):
     # read from the database (get method read from cache)
     # return object read or None if not found
@@ -18,6 +18,59 @@ def get_movie(db: Session, movie_id: int):
 def get_movies(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Movie).offset(skip).limit(limit).all()
 
+def _get_movies_by_predicate(*predicate, db: Session):
+    """ partial request to apply one or more predicate(s) to model Movie"""
+    return db.query(models.Movie)   \
+            .filter(*predicate)
+
+def get_movies_by_title(db: Session, title: str):
+    return _get_movies_by_predicate(models.Movie.title == title, db=db)    \
+            .order_by(desc(models.Movie.year))                      \
+            .all()
+            
+def get_movies_by_title_part(db: Session, title: str):
+    return _get_movies_by_predicate(models.Movie.title.like(f'%{title}%'), db=db)   \
+            .order_by(models.Movie.title, models.Movie.year)                       \
+            .all()
+
+def get_movies_by_year(db: Session, year: int):
+    return _get_movies_by_predicate(models.Movie.year == year, db=db)    \
+            .order_by(models.Movie.title)                         \
+            .all()
+
+def get_movies_by_range_year(db: Session, year_min: Optional[int], year_max: Optional[int]):
+    if year_min is None and year_max is None:
+        return None
+    if year_min is None:
+        predicate = models.Movie.year <= year_max
+    elif year_max is None:
+        predicate = models.Movie.year >= year_min
+    else:
+        predicate = between(models.Movie.year, year_min, year_max)
+    return _get_movies_by_predicate(predicate, db=db)                  \
+            .order_by(models.Movie.year, models.Movie.title)    \
+            .all()
+
+def get_movies_by_title_year(db: Session, title: str, year: int):
+    return _get_movies_by_predicate(
+                    models.Movie.title == title, 
+                    models.Movie.year == year,
+                    db=db)                                      \
+            .order_by(models.Movie.year, models.Movie.title)    \
+            .all()
+
+def get_nb_movies(db: Session):
+    return db.query(models.Movie).count()
+
+def get_nb_movies_by_year(db: Session, year: int):
+    return _get_movies_by_predicate(models.Movie.year == year).count()
+
+
+# CRUD for Star objects
+def _get_stars_by_predicate(*predicate, db: Session):
+    """ partial request to apply one or more predicate(s) to model Star"""
+    return db.query(models.Star)   \
+            .filter(*predicate)    
 
 def get_star(db: Session, star_id: int):
     # read from the database (get method read from cache)
@@ -29,8 +82,18 @@ def get_star(db: Session, star_id: int):
 def get_stars(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Star).offset(skip).limit(limit).all()
 
+def get_stars_by_name(db: Session, name: str):
+    return _get_stars_by_predicate(models.Star.name == name, db=db) \
+            .order_by(models.Star.birthdate)  \
+            .all()
+
+def get_stars_by_endname(db: Session, name: str):
+    return _get_stars_by_predicate(models.Star.name.like(f'%{name}'), db=db) \
+            .order_by(models.Star.birthdate)  \
+            .all()
+
 def get_stars_by_birthyear(db: Session, year: int):
-    return db.query(models.Star).filter(alch.extract('year', models.Star.birthdate) == year) \
+    return _get_stars_by_predicate(extract('year', models.Star.birthdate) == year, db=db) \
             .order_by(models.Star.name)  \
             .all()
 
